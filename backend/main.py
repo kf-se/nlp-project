638ec86ecc7069a9fb831bf18149f68a9b9e14ec
kf-se <sidllub@gmail.com>
2021-12-01 20:28:28 +0100

    started on flask rest api

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,21 @@
 from flask import Flask, make_response
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/api/v2/test_response")
-def test_response():
-    headers = {"Content-Type": "application/json"}
-    return make_response(
-        'Test worked!',
-        200,
-        headers=headers
-    )
+names = {
+    "kf": {"age": 27, "gender": "male"},
+    "bill": {"age": 70, "gender": "male"}
+}
 
-@app.get("/hello")
-def hello():
-    return "Hello world!"
+class HelloWorld(Resource):
+    def get(self, name):
+        return names[name]
+    
+
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 if __name__== "__main__":
-    app.run(port=4000)
+    app.run(debug=True)
--- backend/readme.md
+++ backend/readme.md
@@ -1,5 +1,11 @@
 # Flask
 Build an endpoint that serves a response, i.e. *make_response()*
 
+Determine where virtualenv is:
+pip -V
+
 To start:
 - Execute main.py file
+
+Creating REST-api with flask:
+https://youtu.be/GMppyAPbLYk?t=1671
